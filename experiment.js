d7a4666d2cd8501332e8ca2665f29e646a531159
Dzung Nguyen <dzungng89@gmail.com>
2018-01-25 09:44:32 -0600

    asdf

--- experiment.js
+++ experiment.js
@@ -4,62 +4,81 @@ var experiment = [
         secs: 10 },
     {   
         action: 'Run',
-        secs: 30 },
+        secs: 10 },
     {   
-        action: 'Rest and Get ready',
+        action: 'Rest & Get Ready',
         secs: 10 },        
     {   
         action: 'Walk',
-        secs: 30 },
+        secs: 10 },
     {   
-        action: 'Rest and Get ready',
+        action: 'Rest & Get Ready',
         secs: 10 },  
     {   
         action: 'Jumping Jacks',
-        secs: 30 },
+        secs: 10 },
     {   
-        action: 'Rest',
+        action: 'Rest & Get Ready',
         secs: 10 },
-    {
-        action: 'Rest and Get ready',
-        secs: 10 },
+        
+        
+        
+        
     {   
         action: 'Run',
-        secs: 30 },
+        secs: 10 },
     {   
-        action: 'Rest and Get ready',
+        action: 'Rest & Get Ready',
         secs: 10 },        
     {   
         action: 'Walk',
-        secs: 30 },
+        secs: 10 },
     {   
-        action: 'Rest and Get ready',
+        action: 'Rest & Get Ready',
         secs: 10 },  
     {   
         action: 'Jumping Jacks',
-        secs: 30 },
+        secs: 10 },
     {   
-        action: 'Rest',
+        action: 'Rest & Get Ready',
         secs: 10 },
-    {
-        action: 'Get ready',
-        secs: 10 },
+        
+        
     {   
         action: 'Run',
-        secs: 30 },
+        secs: 10 },
     {   
-        action: 'Rest and Get ready',
+        action: 'Rest & Get Ready',
         secs: 10 },        
     {   
         action: 'Walk',
-        secs: 30 },
+        secs: 10 },
     {   
-        action: 'Rest and Get ready',
+        action: 'Rest & Get Ready',
         secs: 10 },  
     {   
         action: 'Jumping Jacks',
-        secs: 30 },
-    {   
-        action: 'Rest',
         secs: 10 },
+    {   
+        action: 'Rest & Get Ready',
+        secs: 10 },
+        
+    {   
+        action: 'Run',
+        secs: 10 },
+    {   
+        action: 'Rest & Get Ready',
+        secs: 10 },        
+    {   
+        action: 'Walk',
+        secs: 10 },
+    {   
+        action: 'Rest & Get Ready',
+        secs: 10 },  
+    {   
+        action: 'Jumping Jacks',
+        secs: 10 },
+    {   
+        action: 'You are done',
+        secs: 10 }
 ]
