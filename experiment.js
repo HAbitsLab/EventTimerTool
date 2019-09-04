29205733dc323f74a64e24de016e83b5f158d766
Rawanalharbi <harbi.rawan@gmail.com>
2019-09-04 14:32:26 -0500

    updating the experment

--- experiment.js
+++ experiment.js
@@ -1,8 +1,54 @@
 var experiment = [
     {
-        action: 'Eat Hot',
-        secs: 5 },
+        action: 'Calibrate',
+        secs: 15 },
     {   
-        action: 'Drink Hot',
-        secs: 5 },
+        action: 'StirHot',
+        secs: 15 },
+    {   
+        action: 'OpenFidge',
+        secs: 15 },
+    {   
+        action: 'WashHands',
+        secs: 15 },
+    {   
+        action: 'BrushTeeth',
+        secs: 15 },
+    {   
+        action: 'DrinkHot',
+        secs: 15 },
+    {   
+        action: 'EatHot',
+        secs: 15 },
+    {   
+        action: 'DrinkRoom',
+        secs: 15 },
+    {   
+        action: 'DrinkCold',
+        secs: 15 },
+    {   
+        action: 'EatCraker',
+        secs: 15 },
+    {   
+        action: 'EatYogurt',
+        secs: 15 },
+    {   
+        action: 'TypeLaptop',
+        secs: 15 },
+    {   
+        action: 'WatchPhone',
+        secs: 15 },
+    {   
+        action: 'AnswerCall',
+        secs: 15 },
+    {   
+        action: 'TalkSeating',
+        secs: 15 },
+    {   
+        action: 'TalkStanding',
+        secs: 15 },
+    {   
+        action: 'Smoke',
+        secs: 15 },
+        
 ]
